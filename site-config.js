6b9e4ee10da3f9fedd6ac326321f3e656386728e
Mahmoud Abdelwahab <mahmoud3799@gmail.com>
2019-07-30 19:51:53 +0200

    wohooo

---
+++ site-config.js
@@ -0,0 +1,85 @@
+module.exports = {
+  // App Info
+  appstore_link         : null,                                  // Enter App Store URL.
+  playstore_link        : "https://google.com",                                  // Enter Google Play Store URL.
+  google_analytics_ID   : null,                                        // Enter Google Analytics ID or ""
+  presskit_download_link: null,                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
+  video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
+  app_url               : "https://google.com",                            // Domain of your website without path_prefix.
+  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "GuideMe",
+  app_price             : null,
+  app_description       : "Egypt will be your next travel destination",
+  app_keywords          : ["travelling", "Egypt", "Cairo", "GuideMe"],
+
+  // Personal Info
+  your_name              : "Syntax Terminators 🔥",
+  your_link              : null,
+  your_city              : "Cairo,Egypt",
+  email_address          : "mahmoud3799@gmail.com",
+  linkedin_username      : null,
+  facebook_username      : null,
+  instagram_username     : null,
+  twitter_username       : null,
+  github_username        : null,
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "Get To Know Egypt Better",
+      description:
+        "Learn about the different cities you can visit and the different activities you can do ",
+      fontawesome_icon_name: "ankh"
+    },
+    {
+      title: "Plan your trips",
+      description:
+        "Plan your trips from your phone and share them with others.",
+      fontawesome_icon_name: "mobile"
+    },
+    {
+      title: "Discover Different Journeys",
+      description:
+        "Don't like planning? Pick and choose from a wide variety of pre-made trips.",
+        fontawesome_icon_name: "magic"
+    },
+    {
+      title: "Book A Tour Guide",
+      description:
+        "Want to make your travel experience even better? Book a tour guide through our app.",
+        fontawesome_icon_name: "male"
+    },
+    {
+      title: "All The Information You'll Need",
+      description:
+        "We provide all the information you'll need like useful phrases, emergency contact numbers and more.",
+        fontawesome_icon_name: "info"
+    },
+    {
+      title: "Get Recommendations",
+      description:
+        "Want to find out what are the best hotels? Best Spas? Restaurants? We create a list that is frequently updated so that you can have the best possible experience.",
+      fontawesome_icon_name: "hand-holding-heart"
+    }
+  ],
+  header_background             : "rgba(0, 0, 0, 0.1)",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "rgba(54, 59, 61, 0.8)",
+  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "ffffff",
+  primary_text_color            : "#000",
+  content_width                 : "1170px",
+  font                          : `"Helvetica Neue", sans-serif`,
+  link_color                    : "#1d63ea",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_description_color         : "#ffffff",
+  feature_title_color           : "#000000",
+  feature_text_color            : "#666666",
+  feature_icons_foreground_color: "#1d63ea",
+  feature_icons_background_color: "#e6e6e6",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#e6e6e6",
+  footer_text_color             : "#666666",
+}
---
+++ src/components/layout.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import GlobalStyle from "./style.js"
+
+const Layout = ({ children }) => (
+  <>
+    <GlobalStyle />
+    {children}
+  </>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
